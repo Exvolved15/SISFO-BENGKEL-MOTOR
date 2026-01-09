745886691f56faf452ed9d3791f40c8b8fae776e
Exvolved15 <muhfadhilqaulan@gmail.com>
2026-01-09 19:53:40 +0800

    Cleanup: Menghapus folder duplikat, file sampah, dan folder history

--- BENGKEL-MOTOR-main/src/models/motor.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const motorSchema = new mongoose.Schema({
-    name: { type: String, required: true }, // Contoh: Honda Vario 150
-    brand: { type: String, required: true }, // Contoh: Honda
-    type: { type: String, enum: ['Matic', 'Bebek', 'Sport'], default: 'Matic' }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Motor', motorSchema);
